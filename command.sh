4389ec15832403a4dbd80a78d92d52b679b14d0f
Ishwari Upadhyay <ishwariupadhyay85@gmail.com>
2024-01-21 18:46:11 -0500

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,16 @@
+# Install Python and pip 
+sudo apt-get update
+
+sudo apt-get install python3-apt
+sudo python3 get-pip.py
+
+# Install Mage
+sudo pip3 install mage-ai
+
+# Install Pandas
+sudo pip3 install pandas
+
+# Install Google Cloud Library
+sudo pip3 install google-cloud
+
+sudo pip3 install google-cloud-bigquery
